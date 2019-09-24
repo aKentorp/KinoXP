b45e3d529bbbbb89a8359bd766e4519526da4494
oggynator <august.hejberg@gmail.com>
2019-09-24 11:51:47 +0200

    Create show

---
+++ Tests/Repos/showRepoTest.java
@@ -0,0 +1,19 @@
+package Repos;
+
+import Model.Show;
+import org.junit.jupiter.api.Test;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class showRepoTest {
+    private TheaterRepo theaterRepo = new TheaterRepo();
+    private showRepo showRepo = new showRepo();
+
+    @Test
+    void createShow() {
+
+        //TODO test som tjekker om showlisten indeholder objektet Show
+
+    }
+}
--- src/Model/Show.java
+++ src/Model/Show.java
@@ -11,16 +11,17 @@ public class Show {
     private Date showDate;
     private Date startTime;
     private Date endTime;
+    private int remainingSeats;
 
- //  public Show(String title, String genre, int ageLimit, int theaterNumber, Date showDate, Date startTime, Date endTime) {
- //      this.title = title;
- //      this.genre = genre;
- //      this.ageLimit = ageLimit;
- //      this.theaterNumber = theaterNumber;
- //      this.showDate = showDate;
- //      this.startTime = startTime;
- //      this.endTime = endTime;
- //  }
+    public Show(String title, String genre, int ageLimit, int theaterNumber, Date showDate, Date startTime, Date endTime) {
+        this.title = title;
+        this.genre = genre;
+        this.ageLimit = ageLimit;
+        this.theaterNumber = theaterNumber;
+        this.showDate = showDate;
+        this.startTime = startTime;
+        this.endTime = endTime;
+     }
 
     public String getTitle() {
         return title;
@@ -76,5 +77,18 @@ public class Show {
 
     public void setEndTime(Date endTime) {
         this.endTime = endTime;
+    }
+
+    public int getRemainingSeats() {
+        return remainingSeats;
+    }
+
+    public void setRemainingSeats(int remainingSeats) {
+        this.remainingSeats = remainingSeats;
+    }
+
+    @Override
+    public String toString() {
+        return "Title: " + this.title +"\n Genre: " + this.genre + "\n Age limit: " + this.ageLimit + "\n Theater number: " + this.theaterNumber + "\n show date: " + this.showDate + "\n Start time: " + this.startTime + "\n End time: " + this.endTime + "\n Remaining seats: "+ this.remainingSeats;
     }
 }
