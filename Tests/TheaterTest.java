9a9077f3f0c47715cba1237d3e945afd87f1663c
oggynator <august.hejberg@gmail.com>
2019-09-24 09:27:15 +0200

    Junit

--- Tests/TheaterTest.java
+++ Tests/TheaterTest.java
@@ -1,5 +1,7 @@
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TheaterTest {
